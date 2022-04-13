369107592f672ccddb436a62da959a4832fd3215
SwapCode08 <sy47525@gmail.com>
2022-04-13 02:47:00 +0100

    Style and write js script for switching between theme dark/light

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,33 @@ tabs.forEach(tab => {
 })
 
 /*=============== DARK LIGHT THEME ===============*/
+let themeButton = document.getElementById('theme_button')
+let darkTheme = 'dark_theme'
+let iconTheme = 'ri-sun-line'
 
+// Previously selected topic (if user selected)
+let selectedTheme = localStorage.getItem('selected_theme')
+let selectedIcon = localStorage.getItem('selected_icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+let getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+let getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected_theme', getCurrentTheme())
+    localStorage.setItem('selected_icon', getCurrentIcon())
+})
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
