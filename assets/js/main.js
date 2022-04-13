9a7cee15a180157c23d241bfab3aafc418d4b6bd
SwapCode08 <sy47525@gmail.com>
2022-04-13 02:11:17 +0100

    Create active class to make tabs projects and skills work by JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,26 @@
 /*=============== FILTERS TABS ===============*/
+let tabs        = document.querySelectorAll('[data-target]')
+    tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        let target = document.querySelector(tab.dataset.target)
+
+        /*  tc => tabContents */
+        tabContents.forEach(tc => {
+            tc.classList.remove('filters_active')
+        })
+
+        target.classList.add('filters_active')
+
+        /* t => tab */
+        tabs.forEach(t => {
+            t.classList.remove('filters_tab_active')
+        })
+
+        tab.classList.add('filters_tab_active')
+    })
+})
 
 /*=============== DARK LIGHT THEME ===============*/
 
